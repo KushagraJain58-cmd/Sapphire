c53ce38f880626b6e279e11511255bc7a524adcf
Kushagra <ishaanjain112@gmail.com>
2023-06-08 16:51:54 +0530

    Header responsive

--- scripts/script.js
+++ scripts/script.js
@@ -101,3 +101,18 @@ window.addEventListener('scroll', function() {
 		slickDotsElement.classList.remove('hide');
 	}
 });
+
+//////// NAV ICON CLICK /////////
+const navIcon = document.querySelector('.nav-icon');
+const nav = document.querySelector('nav');
+navIcon.addEventListener('click', () => {
+	if (nav.classList.contains('shown')) {
+		nav.classList.remove('shown');
+		navIcon.classList.remove('close');
+		nav.style.height = 0 + 'px';
+	} else {
+		nav.classList.add('shown');
+		navIcon.classList.add('close');
+		nav.style.height = nav.scrollHeight + 150 + 'px';
+	}
+});
